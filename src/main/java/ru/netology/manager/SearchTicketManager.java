9b0b3c68d99184cf1557d521b7fbe4f1ca89fc2c
Valeriya Polukhina <valeriapolyxina@mail.ru>
2021-11-27 14:43:27 +0300

    Uograde code

--- src/main/java/ru/netology/manager/SearchTicketManager.java
+++ src/main/java/ru/netology/manager/SearchTicketManager.java
@@ -4,6 +4,7 @@ import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SearchTicketManager {
     private TicketRepository repository;
@@ -16,15 +17,15 @@ public class SearchTicketManager {
         repository.save(ticket);
     }
 
-    public Ticket[] searchBy(String departureAirport, String arrivalAirport) {
+    public Ticket[] searchBy(String departureAirport, String arrivalAirport, Comparator<Ticket> comparator) {
         Ticket[] result = new Ticket[0];
         for (Ticket ticket : repository.findAll()) {
-            if (ticket.getDepartureAirport().equals(departureAirport) && ticket.getArrivalAirport().equals(arrivalAirport)) {
+            if (ticket.getDepartureAirport().equals(departureAirport) && ticket.getArrivalAirport().equals(arrivalAirport))  {
                 Ticket[] tmp = new Ticket[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = ticket;
                 result = tmp;
-                Arrays.sort(result);
+                Arrays.sort(result, comparator);
             }
         }
         return result;
